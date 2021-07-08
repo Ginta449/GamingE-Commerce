2a70375bb082490fd6508bc30ab222052ce5926b
Ginta <ginta6290@gmail.com>
2021-07-08 20:15:25 +0100

    First Commit

---
+++ Data/Migrations/20210419182618_azureadded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GamingE_Commerce.Data.Migrations
+{
+    public partial class azureadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Azure",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Azure", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Azure");
+        }
+    }
+}
